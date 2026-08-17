0a31a912f98206aacd26d0bb5b801794659def53
LaBMediaGroup <153165277+LaBMediaGroup@users.noreply.github.com>
2026-08-17 19:43:21 -0400

    Events: remove Sci-Gore Oct 17 entry

--- spotlight-data.js
+++ spotlight-data.js
@@ -98,20 +98,6 @@ var SPOTLIGHT = [
     tags:  ['Alger Theater','Detroit Film Theatre','Farmington Civic','Historic Howell','Lowkey Cinema','Marquee Arts','Milford Independent','Senate Theater','Slimeball Cinerama'],
     url:   'https://www.facebook.com/events/2520859208335856'
   },
-  {
-    id:    'sci-gore-2026',
-    date:  '2026-10-17',
-    until: '2026-10-18',
-    time:  '11 AM Sat \u2013 6 PM Sun',
-    title: 'Sci-Gore: <em>Sci-Fi, Horror &amp; Cosplay</em>',
-    by:    'Great Lakes Event Network',
-    byUrl: 'https://www.facebook.com/GreatLakesEventNetwork',
-    with_: 'Josh Wynn',
-    where: '21001 Van Born Rd, Taylor',
-    body:  'A two-day convention built around science fiction, horror and dark fantasy \u2014 themed vendors, special guests, showcases, and cosplay wrestling. What earns it a place on this list is the in-house limited-access theater, with screenings running across both days. Closer to cult cinema and retro convention culture than to a comic hall, and deliberately louder and stranger than a traditional con.',
-    tags:  ['Convention','Screenings','Horror','Sci-fi','Cosplay'],
-    url:   'https://www.facebook.com/events/1559253325559765'
-  }
 ];
 
 if (typeof window !== 'undefined') window.SPOTLIGHT = SPOTLIGHT;
